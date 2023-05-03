106a687e48e04c62f81d2a9ba6272abcb95e911a
Ethan Zhang <zhangeth@usc.edu>
2023-05-02 19:07:45 -0700

    testing select iterator

--- src/CSCI485ClassProject/iterators/ProjectIterator.java
+++ src/CSCI485ClassProject/iterators/ProjectIterator.java
@@ -92,8 +92,10 @@ public class ProjectIterator extends Iterator {
 
                         FDBHelper.setFDBKVPair(dupSubspace, tx1, kvPair);
                         FDBHelper.commitTransaction(tx1);
+                        System.out.println("booty");
                     }
                     else {
+                        System.out.print("yurt");
                         r = recordsImpl.getNext(cursor);
                         continue;
                     }
