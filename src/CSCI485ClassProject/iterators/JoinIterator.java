42a495e37adda3280fe4cec0658396d662578171
Ethan Zhang <zhangeth@usc.edu>
2023-05-05 06:56:48 -0700

    testing select iterator

--- src/CSCI485ClassProject/iterators/JoinIterator.java
+++ src/CSCI485ClassProject/iterators/JoinIterator.java
@@ -162,6 +162,7 @@ public class JoinIterator extends Iterator {
             // loop through all of outer subdir
             for (; currentOuterIdx < pairs.size(); currentOuterIdx++)
             {
+                System.out.println("checking: " + currentOuterIdx + " for: " + rightVal);
                 FDBKVPair p = pairs.get(currentOuterIdx);
                 Object leftVal = p.getKey().get(0);
                 //System.out.println("leftVal: " + leftVal);
